eb0c243359ffc0e448df30b965ab08e293f62096
stevebrown0804 <stevebrown0804@gmail.com>
2023-02-16 18:48:56 -0700

    incorporated the prof's gameState code into Breakout_Game.cs

--- Breakout/Breakout_Game.cs
+++ Breakout/Breakout_Game.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 //TODO: Incorporate the prof's code into this file
 
@@ -23,22 +24,29 @@ namespace Breakout
 
     public class Breakout_Game : Game
     {
-        private GraphicsDeviceManager graphics;
+        //private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
 
+        //the prof's stuff
+        private GraphicsDeviceManager m_graphics;
+        private IGameState m_currentState;
+        private GameStateEnum m_nextStateEnum = GameStateEnum.MainMenu;
+        private Dictionary<GameStateEnum, IGameState> m_states;
+        //END
+
         //Game state stuff
-        GameStateEnum gameStateEnum;
-        IGameState gameState;
+        //GameStateEnum gameStateEnum;
+        //IGameState gameState;
 
         //Subsystems
-        InputProcessor inputProcessor;
-        BO_Keyboard keyboard;
-        Sprites sprites;  //TODO: See if we need to use this AND ContentManager
-        Renderer renderer;
+        //InputProcessor inputProcessor;
+        //BO_Keyboard keyboard;
+        //Sprites sprites;  //TODO: See if we need to use this AND ContentManager
+        //Renderer renderer;
 
         public Breakout_Game()
         {
-            graphics = new GraphicsDeviceManager(this);
+            m_graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -46,9 +54,35 @@ namespace Breakout
         protected override void Initialize()
         {
             //Initialize subsystems!
-            inputProcessor = new();
-            keyboard = new();
-            renderer = new();
+            //inputProcessor = new();
+            //keyboard = new();
+            //renderer = new();
+
+            //the prof's code
+            m_graphics.PreferredBackBufferWidth = 1920;
+            m_graphics.PreferredBackBufferHeight = 1080;
+
+            m_graphics.ApplyChanges();
+
+            // Create all the game states here
+            m_states = new Dictionary<GameStateEnum, IGameState>
+            {
+                { GameStateEnum.MainMenu, new MainMenuView() },
+                { GameStateEnum.GamePlay, new GamePlayView() },
+                { GameStateEnum.HighScores, new HighScoresView() },
+                { GameStateEnum.Help, new HelpView() },
+                { GameStateEnum.About, new AboutView() }
+            };
+
+            // Give all game states a chance to initialize, other than constructor
+            foreach (var item in m_states)
+            {
+                item.Value.initialize(this.GraphicsDevice, m_graphics);
+            }
+
+            // We are starting with the main menu - as defined by the value set in m_nextStateEnum
+            m_currentState = m_states[m_nextStateEnum];
+            //END the prof's code
 
             base.Initialize();
         }
@@ -59,18 +93,37 @@ namespace Breakout
 
             // TODO: use this.Content to load your game content here
 
-            sprites = new(this, spriteBatch);
+            //sprites = new(this, spriteBatch);
             //...then use sprites' dictionar(y/ies) to get Texture2Ds
+
+            //the prof's code
+            // Give all game states a chance to load their content
+            foreach (var item in m_states)
+            {
+                item.Value.loadContent(this.Content);
+            }
+            //END
         }
 
         protected override void Update(GameTime gameTime)
         {
-            inputProcessor.ProcessInput(gameTime, keyboard);
+            //inputProcessor.ProcessInput(gameTime, keyboard);
 
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+
+            //The prof's code
+            m_nextStateEnum = m_currentState.processInput(gameTime);
+            // Special case for exiting the game
+            if (m_nextStateEnum == GameStateEnum.Exit)
+            {
+                Exit();
+            }
+
+            m_currentState.update(gameTime);
+            //END
 
             base.Update(gameTime);
         }
@@ -81,8 +134,16 @@ namespace Breakout
 
             // TODO: Add your drawing code here
             //  Retort: Ok!
-            renderer.DoRendering();
-          
+            //renderer.DoRendering();
+
+            //The prof's code
+            GraphicsDevice.Clear(Color.Black);
+
+            m_currentState.render(gameTime);
+
+            m_currentState = m_states[m_nextStateEnum];
+            //END
+
 
             base.Draw(gameTime);
         }
--- Breakout/Game states/MainMenuView.cs
+++ Breakout/Game states/MainMenuView.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Breakout;
 
 namespace Breakout.Game_states
 {
